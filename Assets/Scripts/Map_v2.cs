4c45da2bc44f77083b2c98cc54060d120ec810aa
Tetsuya Saito <>
2023-07-11 01:09:06 +0900

    部屋を作るところまで

--- Assets/Scripts/Map_v2.cs
+++ Assets/Scripts/Map_v2.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Map_v2 : MonoBehaviour
 {
+    public Transform parentFloor;
+    public Transform parentRealms;
     int mapSizeX = 40;
     int mapSizeY = 30;
     private GameObject floorPrefab;
@@ -12,66 +14,90 @@ public class Map_v2 : MonoBehaviour
     // public int[] realms;
     public int[,] map;
     public int setId = 0;
-    public List<RectBox> realms = new List<RectBox>();
-    private int minRoomSizeX = 10;
-    private int minRoomSizeY = 10;
-    private float testRealmsCount = 0;//後で消す
-    private int minArea = 10;
-    private int minWidth = 5;
-    private int minHeight = 5;
+    public List<Realm> realms = new List<Realm>();
+    // private int minRoomSizeX = 10;
+    // private int minRoomSizeY = 10;
+    private int testRealmsCount = 0;//後で消す
+    private int minArea = 50;
+    private int minWidth = 8;
+    private int maxWidth = 0;
+    private int minHeight = 8;
+    private int maxHeight = 0;
+    private int margin = 1;
+    private int minRatio = 40;//分割した区画の何%を最小の部屋にするか
+    private int roomMinusRange = 3;//
     // Start is called before the first frame update
+    private string[] colorArray = new string[]
+        {
+            "#B73500",
+            "#FF6464",
+            "#FF0055",
+            "#FF00B8",
+            "#C200FF",
+            "#5E00FF",
+            "#001EFF",
+            "#0088FF",
+            "#00F8FF",
+            "#00FF8E",
+            "#C6FF00"
+        }
+    ;
     void Start()
     {
         map = new int[mapSizeX, mapSizeY];
+        maxWidth = (int)mapSizeX / 3 * 2;
+        maxHeight = (int)mapSizeY / 3 * 2;
         //初期値
-        realms.Add(new RectBox(0, 0, mapSizeX, mapSizeY));
+        realms.Add(new Realm(0, 0, mapSizeX, mapSizeY));
         floorPrefab = Resources.Load("Prefabs/Floor") as GameObject;
         wallPrefab = Resources.Load("Prefabs/Wall") as GameObject;
 
         GenerateMap();
     }
-    private void GenerateMap()
+    public void GenerateMap()
     {
         divideRoom();
+        MakeRoom();
+        // CreatRoom();
     }
     public void divideRoom(){
         separateLargestRealm();
-        RectBox firstRealm = new RectBox();
-        RectBox secondRealm = new RectBox();
+        Realm firstRealm = new Realm();
+        Realm secondRealm = new Realm();
         firstRealm = realms[realms.Count - 2];
         secondRealm = realms[realms.Count - 1];
 
         GameObject _obj;
         SpriteRenderer spriteRenderer;
         // 新しいHSV値を設定する
-        float hue = 0f;        // 色相（0.0 - 1.0）
-        float saturation = 0.8f; // 彩度（0.0 - 1.0）
-        float value = 0.5f;      // 明度（0.0 - 1.0）
-        testRealmsCount += 0.1f;
-        hue = testRealmsCount;
+        // float hue = 0f;        // 色相（0.0 - 1.0）
+        // float saturation = 0.8f; // 彩度（0.0 - 1.0）
+        // float value = 0.5f;      // 明度（0.0 - 1.0）
+        testRealmsCount++;
+        // hue = testRealmsCount;
         int test_creat_count = 0;
         string test_a1 = "";
         string test_a2 = "";
         for (int y = firstRealm.top; y < firstRealm.top + firstRealm.sizeY; y++){
             for (int x = firstRealm.left; x < firstRealm.left + firstRealm.sizeX; x++){
-                _obj = Instantiate(wallPrefab, new Vector3(x, y, 0), new Quaternion());      
+                _obj = Instantiate(wallPrefab, new Vector3(x, y, 0), new Quaternion(),parentRealms);      
                 spriteRenderer = _obj.GetComponent<SpriteRenderer>();              
                 // 新しい色を作成する
-                Color newColor = Color.HSVToRGB(hue, saturation, value);
+                Color newColor = ColorUtility.TryParseHtmlString(colorArray[testRealmsCount], out Color result) ? result : Color.white;
                 spriteRenderer.color = newColor; // 好みの色に変更する
                 test_creat_count++;
                 test_a1 += "1,";
             }         
             test_a1 += "\n";       
         }
-        testRealmsCount += 0.1f;
-        hue = testRealmsCount;
+        testRealmsCount++;
+        // hue = testRealmsCount;
         for (int y = secondRealm.top; y < secondRealm.top + secondRealm.sizeY; y++){
             for (int x = secondRealm.left; x < secondRealm.left + secondRealm.sizeX; x++){
-                _obj = Instantiate(wallPrefab, new Vector3(x, y, 0), new Quaternion());      
+                _obj = Instantiate(wallPrefab, new Vector3(x, y, 0), new Quaternion(),parentRealms);      
                 spriteRenderer = _obj.GetComponent<SpriteRenderer>();
                 // 新しい色を作成する
-                Color newColor = Color.HSVToRGB(hue, saturation, value);
+                Color newColor = ColorUtility.TryParseHtmlString(colorArray[testRealmsCount], out Color result) ? result : Color.white;
                 spriteRenderer.color = newColor; // 好みの色に変更する   
                 test_creat_count++;
                 test_a2 += "2,";
@@ -81,16 +107,16 @@ public class Map_v2 : MonoBehaviour
         DebugRealms(realms);
         Debug.Log("分割数=" + realms.Count);    
     }
-    private List<RectBox> RemoveRealms(List<RectBox> _realms, int _removeId = 0){
-        List<RectBox> new_realms = new List<RectBox>();
-        foreach(RectBox _rect in _realms){
+    private List<Realm> RemoveRealms(List<Realm> _realms, int _removeId = 0){
+        List<Realm> new_realms = new List<Realm>();
+        foreach(Realm _rect in _realms){
             if(_rect.id != _removeId){
                 new_realms.Add(_rect);
             }
         }
         return new_realms;
     }
-    private void DebugRealms(List<RectBox> _realms){
+    private void DebugRealms(List<Realm> _realms){
         int[,] checkArr = new int[mapSizeY, mapSizeX];
         for (int y = 0; y < checkArr.GetLength(0); y++)
         {
@@ -98,7 +124,7 @@ public class Map_v2 : MonoBehaviour
                 checkArr[y, x] = 0;
             }
         }
-        foreach(RectBox r in _realms){
+        foreach(Realm r in _realms){
             for (int y = r.top; y < r.top + r.sizeY; y++){
                 for (int x = r.left; x < r.left + r.sizeX; x++)
                 {
@@ -127,8 +153,7 @@ public class Map_v2 : MonoBehaviour
         
         if (index >= 0) {
          
-            RectBox r = getRealmFromId(realms,index);
-            Debug.Log("r.sizeX=" + r.sizeX + "/r.sizeY=" + r.sizeY);
+            Realm r = getRealmFromId(realms,index);
             if (r.sizeX >= r.sizeY) {
             
                 separateRealmByX(index);
@@ -139,13 +164,15 @@ public class Map_v2 : MonoBehaviour
                 
             }
             
+        }else{
+            Debug.LogError("分割できませんでした");
         }
     }
     private bool separateRealmByX(int id) {
         int minWidthByArea = 0;
         int separateX;
 
-        RectBox r = getRealmFromId(realms,id);
+        Realm r = getRealmFromId(realms,id);
 
         if (r.sizeY * (int)Math.Floor((double)r.sizeX / 2) < minArea) {
             Debug.Log("分割できる領域が"+minArea+"未満です。サイズ="+r.sizeY * (int)Math.Floor((double)r.sizeX / 2));
@@ -176,10 +203,10 @@ public class Map_v2 : MonoBehaviour
         }
         realms = RemoveRealms(realms,id);
         //	分割位置を決定
-        RectBox firstRoom = new RectBox(r.left, r.top, separateX, r.sizeY);
+        Realm firstRoom = new Realm(r.left, r.top, separateX, r.sizeY);
         setId++;
         firstRoom.id = setId;
-        RectBox secondRoom = new RectBox(r.left + separateX,r.top,r.sizeX - separateX, r.sizeY);
+        Realm secondRoom = new Realm(r.left + separateX,r.top,r.sizeX - separateX, r.sizeY);
         setId++;
         secondRoom.id = setId;
         realms.Add(firstRoom);
@@ -192,13 +219,13 @@ public class Map_v2 : MonoBehaviour
         int minHeightByArea = 0;
         int separateY;
 
-        RectBox r = getRealmFromId(realms,id);
+        Realm r = getRealmFromId(realms,id);
 
-        if (r.sizeY * (int)Math.Floor((double)r.sizeX / 2) < minArea) {
+        if (r.sizeY * (int)Math.Floor((double)r.sizeY / 2) < minArea) {
             Debug.Log("分割できる領域が"+minArea+"未満です。サイズ="+r.sizeY * (int)Math.Floor((double)r.sizeX / 2));
             return false;
         }
-        if((int)Math.Floor((double)r.sizeX / 2) <= minHeight){
+        if((int)Math.Floor((double)r.sizeY / 2) <= minHeight){
             Debug.Log("sizeYが小さすぎます。sizeY="+r.sizeY);
             return false;
         }
@@ -223,10 +250,10 @@ public class Map_v2 : MonoBehaviour
         }
         realms = RemoveRealms(realms,id);
         //	分割位置を決定
-        RectBox firstRoom = new RectBox(r.left, r.top, r.sizeX, separateY);
+        Realm firstRoom = new Realm(r.left, r.top, r.sizeX, separateY);
         setId++;
         firstRoom.id = setId;
-        RectBox secondRoom = new RectBox(r.left,r.top + separateY, r.sizeX, r.sizeY - separateY);
+        Realm secondRoom = new Realm(r.left,r.top + separateY, r.sizeX, r.sizeY - separateY);
         setId++;
         secondRoom.id = setId;
         realms.Add(firstRoom);
@@ -243,13 +270,13 @@ public class Map_v2 : MonoBehaviour
             area,
             maxArea;
 
-        RectBox r;
+        Realm r;
 
         maxIndex = 0;
         area = 0;
         maxArea = 0;
 
-        if (realms.Count == 0) { return 0; }
+        if (realms.Count == 0) { return -1; }
         
         for (i = 0; i < realms.Count; i += 1) {
 
@@ -268,10 +295,10 @@ public class Map_v2 : MonoBehaviour
         return maxIndex;
 
     }
-    private RectBox getRealmFromId(List<RectBox> _realms, int _id)
+    private Realm getRealmFromId(List<Realm> _realms, int _id)
     {
         // int GetId = 0;
-        RectBox _RectBox = null;
+        Realm _RectBox = null;
         for (int i = 0; i < _realms.Count; i += 1)
         {
             if(_realms[i].id == _id){
@@ -280,9 +307,93 @@ public class Map_v2 : MonoBehaviour
         }
         return _RectBox;
     }
+    public void MakeRoom(){
+        for (int i = 0; i < realms.Count; i++){
+            Realm r = realms[i];
+            if (r.sizeX * r.sizeY < minArea)
+            {
+                r.setRoom(r.left + margin, r.top + margin, r.sizeX - margin * 2, r.sizeY - margin * 2);
+            }else{
+
+                System.Random random = new System.Random();
+                int max_room_size = r.sizeX * r.sizeY;
+
+                int rWidth = 0;
+                int _minusWidth = (int)(random.Next(0, roomMinusRange + 1));
+                rWidth = r.sizeX - _minusWidth;
+                if(rWidth <= minWidth){
+                    rWidth = minWidth - margin * 2;
+                }else{
+                    if(rWidth >= maxWidth){
+                        rWidth = maxWidth - margin * 2;
+                    }else{
+                        rWidth = rWidth - margin * 2;
+                    }
+                }
+
+                int _minusHeight = (int)(random.Next(0, roomMinusRange + 1));
+                int rHeight = r.sizeY - _minusHeight;
+                if(rHeight <= minHeight){
+                    rHeight = minHeight - margin * 2;
+                }else{
+                    if (rHeight >= maxHeight)
+                    {
+                        rHeight = maxHeight - margin * 2;
+                    }else{
+                        rHeight = rHeight - margin * 2;
+                    }
+                }
+
+                System.Random randomLeft = new System.Random();
+                int StartLeft = (int)(randomLeft.Next(0, r.sizeX - margin * 2 - rWidth + 1)) + r.left + margin;
+
+                System.Random randomTop = new System.Random();
+
+                int StartTop = (int)(randomTop.Next(0, r.sizeY - margin * 2- rHeight + 1)) + r.top + margin;
+
+                Debug.Log("ベース部屋サイズ="+max_room_size+"/部屋サイズ="+ rWidth * rHeight);
+                Debug.Log("/r.sizeX=" + r.sizeX + "/r.sizeY=" + r.sizeY+"/rWidth="+rWidth+"/rHeight="+rHeight);
+                Debug.Log("r.left="+r.left+"/r.top="+r.top+"/StartLeft=" + StartLeft + "/StartTop=" + StartTop);
+                Debug.Log("_minusWidth=" + _minusWidth + "/_minusHeight=" + _minusHeight);
+
+                r.setRoom(StartLeft, StartTop, StartLeft + rWidth, StartTop + rHeight);
+
+            }
+        }
+        CreatRoom();
+    }
+    private void CreatRoom(){
+        Debug.Log("realms.Count" + realms.Count);     
+        //一旦子要素は全て削除
+        for (int i = parentFloor.childCount - 1; i >= 0; i--)
+        {
+            Transform child = parentFloor.GetChild(i);
+            Destroy(child.gameObject);
+        }   
+        foreach(Realm _r in realms){
+            // int x = _r.RoomLeft;
+            // int y = _r.RoomTop;
+            // Debug.Log("x=" + x + "/y=" + y);
+            Debug.Log("RoomLeft=" + _r.RoomLeft + "/RoomTop=" + _r.RoomTop + "/RoomRight=" + _r.RoomRight + "/RoomBottom=" + _r.RoomBottom);
+
+            for (int y = _r.RoomTop; y < _r.RoomBottom; y++){
+                for (int x = _r.RoomLeft; x < _r.RoomRight; x++)
+                {
+                    GameObject _obj = Instantiate(wallPrefab, new Vector3(x, y, 0), new Quaternion(),parentFloor);
+                }
+            }
+        }
+        // spriteRenderer = _obj.GetComponent<SpriteRenderer>();              
+        // // 新しい色を作成する
+        // Color newColor = Color.HSVToRGB(hue, saturation, value);
+        // spriteRenderer.color = newColor; // 好みの色に変更する
+        // test_creat_count++;
+        // test_a1 += "1,";       
+    }
+
 
 }
-public class RectBox
+public class Realm
 {
 
     public int sizeX= 0;
@@ -290,9 +401,13 @@ public class RectBox
     public int left = 0;
     public int top = 0;
     public int id = 0;
+    public int RoomLeft = 0;
+    public int RoomRight = 0;
+    public int RoomTop = 0;
+    public int RoomBottom = 0;
 
     // public int R_Y = 0;
-    public RectBox(int _left, int _top, int _sizeX,int _sizeY)
+    public Realm(int _left, int _top, int _sizeX,int _sizeY)
     {
         this.left = _left;
         this.top = _top;
@@ -301,5 +416,11 @@ public class RectBox
         
     }
     //コンストラクタが呼び出し初期化
-    public RectBox() : this(0, 0, 0, 0) {}
+    public Realm() : this(0, 0, 0, 0) {}
+    public void setRoom(int _left, int _top, int _width, int _height){
+        this.RoomLeft = _left;
+        this.RoomTop = _top;
+        this.RoomRight = _width;//-1にする？
+        this.RoomBottom = _height;//-1にする？
+    }
 }
